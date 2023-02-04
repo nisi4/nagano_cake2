3e6e34dbb531aef01f73ec5a3928a02577d0bc1d
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-02-04 14:36:39 +0000

    [Add]enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum order_status: { payment_waiting: 0,payment_confirmation: 1,in_production: 2,preparing_delivery: 3,delivered: 4 }
+  enum product_status: { start_not_possible: 0,production_pending: 1,in_production: 2,production_complete: 3 }
 end

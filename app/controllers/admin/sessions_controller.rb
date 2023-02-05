0437d952d3093da1d52b6edf63f957d2f9f1707b
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-02-05 05:56:18 +0000

    [Fix]log_in form

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,31 +1,32 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
+  # before_action :configure_sign_in_params, only: [:create]
   
   def after_sign_in_path_for(resource)
-    root_path
+    admin_root_path
   end
   
   # GET /resource/sign_in
-  def new
-    super
-  end
+  # def new
+  #   super
+  # end
 
-  # POST /resource/sign_in
-  def create
-    super
-  end
+  # # POST /resource/sign_in
+  # def create
+  #   super
+  # end
 
-  # DELETE /resource/sign_out
+  # # DELETE /resource/sign_out
   def destroy
     super
+    redirect_to new_admin_session_path
   end
 
-  protected
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  end
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end

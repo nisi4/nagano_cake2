4bb328e187be3f697f22a4c883233a41878ab496
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-02-05 11:46:47 +0000

    [start]admin_item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
+    resources :items,only: [:new,:create,:index,:show,:edit,:update]
+  end
+  namespace :admin do
     resources :genres,only: [:index,:create,:edit,:update]
   end
   namespace :admin do

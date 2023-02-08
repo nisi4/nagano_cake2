6a14e3d5c9d1c24979bb718422b16dd8c84a36fe
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-02-08 14:46:18 +0000

    [Fix]image,paginate

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,13 @@ class Item < ApplicationRecord
       (self.price * 1.10).round
    end
    
+   def get_image
+     unless image.attached?
+        file_path = Rails.root.join("app/assets/images/no_image.jpeg")
+        image.attach(io: File.open(file_path), filename:"default-image.jpg", content_type: "image/jpeg")
+     end
+     image
+   end
+   
    belongs_to :genre
 end

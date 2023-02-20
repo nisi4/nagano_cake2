625ae5afe13d49bb34cff8a23ba0e68e7953b2ce
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-02-20 14:51:07 +0000

    [Add]order#confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,9 +4,11 @@ class Public::OrdersController < ApplicationController
   end
   
   def confirm
-    binding.pry
-    
-    
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.full_name
   end
 
   def complete

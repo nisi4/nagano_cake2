3c8285becc8f15d583519d15698e5f481b9161e5
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-02-22 15:34:43 +0000

    [Add]orders#confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,11 +4,22 @@ class Public::OrdersController < ApplicationController
   end
   
   def confirm
-    @order = Order.new(order_params)
-    @address = Address.find(params[:order][:address_id])
-    @order.postal_code = @address.postal_code
-    @order.address = @address.address
-    @order.name = @address.full_name
+    select_address = params[:select_address]
+    if select_address == 0
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.full_name
+    elsif select_address == 1
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    else
+      @order = Order.new(order_params)
+    end
+    @cart_items = current_customer.cart_items
+    @total = 0
+    @postage = 800
   end
 
   def complete

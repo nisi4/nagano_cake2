9eb0e315f956a2fedc12e4c9688ef12c67541f14
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-02-23 05:28:03 +0000

    [Finish]注文情報確認画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,14 +5,15 @@ class Public::OrdersController < ApplicationController
   
   def confirm
     @order = Order.new(order_params)
-    if  @address = Address.find(params[:order][:address_id])
-      @order.postal_code = @address.postal_code
-      @order.address = @address.address
-      @order.name = @address.name
-    elsif @order.address.empty?
+    if params[:order][:select_address] == "0"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.full_name
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
     else
       
     end

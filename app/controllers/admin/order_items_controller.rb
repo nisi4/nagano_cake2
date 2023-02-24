9c3406ee0d7f913d4420c7161bf9522af65d947b
EC2 Default User <ec2-user@ip-172-31-2-229.ap-northeast-1.compute.internal>
2023-02-24 15:00:51 +0000

    [Fix]admin注文履歴一覧

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,6 +1,13 @@
 class Admin::OrderItemsController < ApplicationController
   def update
-    order_item = Order_item.find(params[:id])
-    order_item.update()
+     order_item = OrderItem.find(params[:id])
+     order_item.update(order_item_params)
+     redirect_to admin_order_path(order_id.id)
   end
+  
+  private
+  def order_item_params
+     params.require(:order_item).permit(:product_status)
+  end
+   
 end
